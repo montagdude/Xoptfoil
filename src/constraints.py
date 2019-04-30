06e5a90dd869506e4239b87cc16cc77a036e8e8b
Daniel Prosser <dpross1100@msn.com>
2019-04-30 00:02:25 -0400

    Finish constraints and constraints dialog

--- src/constraints.py
+++ src/constraints.py
@@ -2,10 +2,10 @@ import sys
 
 class Constraints():
     def __init__(self):
-        self.minThickness = 6.0
-        self.maxThickness = 1000.0
-        self.minCamber = -10.0
-        self.maxCamber = 10.0
+        self.minThickness = 0.06
+        self.maxThickness = 100.0
+        self.minCamber = -0.1
+        self.maxCamber = 0.1
         self.minTEAngle = 3.0
         self.checkCurvature = True
         self.maxReverseTop = 0
@@ -16,6 +16,8 @@ class Constraints():
         self.maxFlapAngle = 20.
         self.momentConstraintType = []
         self.minMoment = []
+        self.cpConstraintType = []
+        self.minCp = []
         self.addThickX = []
         self.addThickMin = []
         self.addThickMax = []
@@ -23,7 +25,7 @@ class Constraints():
     def numMomentConstraints(self):
         return len(self.momentConstraintType)
 
-    def addMomentConstraint(constraint_type, min_moment=None):
+    def addMomentConstraint(self, constraint_type, min_moment=None):
         self.momentConstraintType.append(constraint_type)
         if constraint_type == "Specify":
             if min_moment is None:
@@ -33,12 +35,38 @@ class Constraints():
         else:
             self.minMoment.append(0.0)
 
+    def clearMomentConstraints(self):
+        self.momentConstraintType = []
+        self.minMoment = []
+
+    def numCpConstraints(self):
+        return len(self.cpConstraintType)
+
+    def addCpConstraint(self, constraint_type, min_cp=None):
+        self.cpConstraintType.append(constraint_type)
+        if constraint_type == "Specify":
+            if min_cp is None:
+                sys.stderr.write("min_cp must be provided for specified constraint type.\n")
+                sys.exit(1)
+            self.minCp.append(min_cp)
+        else:
+            self.minCp.append(0.0)
+
+    def clearCpConstraints(self):
+        self.cpConstrainteType = []
+        self.minCp = []
+
     def numThicknessConstraints(self):
         return len(self.addThickX)
 
-    def addThicknessConstraint(x, minthick, maxthick):
+    def addThicknessConstraint(self, x, minthick, maxthick):
         self.addThickX.append(x)
         self.addThickMin.append(minthick)
         self.addThickMax.append(maxthick)
+
+    def clearThicknessConstraints(self):
+        self.addThickX = []
+        self.addThickMin = []
+        self.addThickMax = []
 
 constraints = Constraints()
--- src/constraints_dialog.py
+++ src/constraints_dialog.py
@@ -18,39 +18,111 @@ class ConstraintsDialog(QDialog):
                                                   QHeaderView.ResizeToContents)
 
         # Signals and slots
+        self.ui.checkCurvatureBox.stateChanged[int].connect(self.setItemsEnabled)
         self.ui.addButton.clicked.connect(self.addThicknessConstraint)
         self.ui.deleteButton.clicked.connect(self.deleteThicknessConstraint)
 
-        # Sets up moment constraints table with default values
+        # Sets up moment constraints table
         self.momenttype_boxes = []
         self.minmoment_boxes = []
         self.setupMomentTable()
+
+        # Sets up cp constraints table
+        self.cptype_boxes = []
+        self.mincp_boxes = []
+        self.setupCpTable()
 
         # Additional thickness constraints widgets
         self.addthickx_boxes = []
         self.addthickmin_boxes = []
         self.addthickmax_boxes = []
 
-    # Sets up moment constraints table with default values
+        # Populate items
+        self.populate()
+
+    # Populates items
+    def populate(self):
+        self.ui.minThicknessBox.setValue(constraints.minThickness*100.)
+        self.ui.maxThicknessBox.setValue(constraints.maxThickness*100.)
+        self.ui.minCamberBox.setValue(constraints.minCamber*100.)
+        self.ui.maxCamberBox.setValue(constraints.maxCamber*100.)
+        self.ui.minTEAngleBox.setValue(constraints.minTEAngle)
+        self.ui.checkCurvatureBox.setChecked(constraints.checkCurvature)
+        self.ui.maxReverseTopBox.setValue(constraints.maxReverseTop)
+        self.ui.maxReverseBottomBox.setValue(constraints.maxReverseBot)
+        self.ui.curveThresholdBox.setValue(constraints.curveThreshold)
+        self.ui.symmetricalBox.setChecked(constraints.symmetrical)
+        self.ui.minFlapAngleBox.setValue(constraints.minFlapAngle)
+        self.ui.maxFlapAngleBox.setValue(constraints.maxFlapAngle)
+
+        # Min moment boxes. Make consistent with number of operating points, then set values.
+        numpoints = operatingpoints.numPoints()
+        if len(self.momenttype_boxes) > numpoints:
+            self.momenttype_boxes = self.momenttype_boxes[0:numpoints]
+            self.minmoment_boxes = self.minmoment_boxes[0:numpoints]
+        elif len(self.momenttype_boxes) < numpoints:
+            nadd = numpoints - len(self.momenttype_boxes)
+            for i in range(nadd):
+                self.appendMomentBoxes()
+        for i in range(min(constraints.numMomentConstraints(),numpoints)):
+            self.momenttype_boxes[i].setCurrentText(constraints.momentConstraintType[i])
+            self.minmoment_boxes[i].setValue(constraints.minMoment[i])
+
+        # Min Cp boxes. Treat same way as moment boxes.
+        if len(self.cptype_boxes) > numpoints:
+            self.cptype_boxes = self.cptype_boxes[0:numpoints]
+            self.mincp_boxes = self.mincp_boxes[0:numpoints]
+        elif len(self.cptype_boxes) < numpoints:
+            nadd = numpoints - len(self.cptype_boxes)
+            for i in range(nadd):
+                self.appendCpBoxes()
+        for i in range(min(constraints.numCpConstraints(),numpoints)):
+            self.cptype_boxes[i].setCurrentText(constraints.cpConstraintType[i])
+            self.mincp_boxes[i].setValue(constraints.minCp[i])
+
+        # Thickness constraints
+        for i in range(constraints.numThicknessConstraints()):
+            self.addThicknessConstraint()
+            self.addthickx_boxes[i].setValue(constraints.addThickX[i])
+            self.addthickmin_boxes[i].setValue(constraints.addThickMin[i]*100.)
+            self.addthickmax_boxes[i].setValue(constraints.addThickMax[i]*100.)
+
+    # Enables/disables items
+    def setItemsEnabled(self, choice):
+        if choice == 2:
+            self.ui.maxReverseTopBox.setEnabled(True)
+            self.ui.maxReverseBottomBox.setEnabled(True)
+            self.ui.curveThresholdBox.setEnabled(True)
+        elif choice == 0:
+            self.ui.maxReverseTopBox.setEnabled(False)
+            self.ui.maxReverseBottomBox.setEnabled(False)
+            self.ui.curveThresholdBox.setEnabled(False)
+
+    # Appends to list of moment widgets using default values
+    def appendMomentBoxes(self):
+        combobox = QComboBox(self.ui.momentTable)
+        combobox.addItem("None")
+        combobox.addItem("Use seed")
+        combobox.addItem("Specify")
+        spinbox = QDoubleSpinBox(self.ui.momentTable)
+        spinbox.setDecimals(4)
+        spinbox.setMaximum(10.0)
+        spinbox.setMinimum(-10.0)
+        spinbox.setSingleStep(0.01)
+        spinbox.setValue(-0.1)
+        spinbox.setEnabled(False)
+        self.momenttype_boxes.append(combobox)
+        self.minmoment_boxes.append(spinbox)
+        nmoment = len(self.momenttype_boxes)
+        self.momenttype_boxes[nmoment-1].currentIndexChanged[str].connect(
+                                         self.setMomentBoxesEnabled)
+
+    # Sets up moment constraints table using default values
     def setupMomentTable(self):
         self.momenttype_boxes = []
         self.minmoment_boxes = []
         for i in range(operatingpoints.numPoints()):
-            combobox = QComboBox(self.ui.momentTable)
-            combobox.addItem("None")
-            combobox.addItem("Use seed")
-            combobox.addItem("Specify")
-            spinbox = QDoubleSpinBox(self.ui.momentTable)
-            spinbox.setDecimals(4)
-            spinbox.setMaximum(10.0)
-            spinbox.setMinimum(-10.0)
-            spinbox.setSingleStep(0.01)
-            spinbox.setValue(-0.1)
-            spinbox.setEnabled(False)
-            self.momenttype_boxes.append(combobox)
-            self.minmoment_boxes.append(spinbox)
-            self.momenttype_boxes[i].currentIndexChanged[str].connect(
-                                     self.setMomentBoxesEnabled)
+            self.appendMomentBoxes()
             self.ui.momentTable.setRowCount(i+1)
             self.ui.momentTable.setItem(i, 0, QTableWidgetItem("{:d}".format(i+1)))
             self.ui.momentTable.setCellWidget(i, 1, self.momenttype_boxes[i])
@@ -64,6 +136,43 @@ class ConstraintsDialog(QDialog):
                 self.minmoment_boxes[i].setEnabled(True)
             else:
                 self.minmoment_boxes[i].setEnabled(False)
+
+    # Appends to list of Cp widgets using default values
+    def appendCpBoxes(self):
+        combobox = QComboBox(self.ui.cpTable)
+        combobox.addItem("None")
+        combobox.addItem("Specify")
+        spinbox = QDoubleSpinBox(self.ui.cpTable)
+        spinbox.setDecimals(4)
+        spinbox.setMaximum(0.0)
+        spinbox.setMinimum(-99.99)
+        spinbox.setSingleStep(0.01)
+        spinbox.setValue(-2.0)
+        spinbox.setEnabled(False)
+        self.cptype_boxes.append(combobox)
+        self.mincp_boxes.append(spinbox)
+        ncp = len(self.cptype_boxes)
+        self.cptype_boxes[ncp-1].currentIndexChanged[str].connect(self.setCpBoxesEnabled)
+
+    # Sets up Cp constraints table with default values
+    def setupCpTable(self):
+        self.cptype_boxes = []
+        self.mincp_boxes = []
+        for i in range(operatingpoints.numPoints()):
+            self.appendCpBoxes()
+            self.ui.cpTable.setRowCount(i+1)
+            self.ui.cpTable.setItem(i, 0, QTableWidgetItem("{:d}".format(i+1)))
+            self.ui.cpTable.setCellWidget(i, 1, self.cptype_boxes[i])
+            self.ui.cpTable.setCellWidget(i, 2, self.mincp_boxes[i])
+        self.ui.cpTable.verticalHeader().setVisible(False)
+
+    # Sets min Cp boxes enabled or disabled
+    def setCpBoxesEnabled(self):
+        for i in range(len(self.cptype_boxes)):
+            if self.cptype_boxes[i].currentText() == "Specify":
+                self.mincp_boxes[i].setEnabled(True)
+            else:
+                self.mincp_boxes[i].setEnabled(False)
 
     # Adds a thickness constraint
     def addThicknessConstraint(self):
@@ -109,4 +218,37 @@ class ConstraintsDialog(QDialog):
             self.ui.deleteButton.setEnabled(False)
 
     def saveConstraints(self):
-        pass
+        constraints.minThickness = self.ui.minThicknessBox.value()/100.
+        constraints.maxThickness = self.ui.maxThicknessBox.value()/100.
+        constraints.minCamber = self.ui.minCamberBox.value()/100.
+        constraints.maxCamber = self.ui.maxCamberBox.value()/100.
+        constraints.minTEAngle = self.ui.minTEAngleBox.value()
+        constraints.checkCurvature = self.ui.checkCurvatureBox.isChecked()
+        constraints.maxReverseTop = self.ui.maxReverseTopBox.value()
+        constraints.maxReverseBot = self.ui.maxReverseBottomBox.value()
+        constraints.curveThreshold = self.ui.curveThresholdBox.value()
+        constraints.symmetrical = self.ui.symmetricalBox.isChecked()
+        constraints.minFlapAngle = self.ui.minFlapAngleBox.value()
+        constraints.maxFlapAngle = self.ui.maxFlapAngleBox.value()
+
+        # Moment constraints
+        constraints.clearMomentConstraints()
+        for i in range(len(self.momenttype_boxes)):
+            constraint_type = self.momenttype_boxes[i].currentText()
+            min_moment = self.minmoment_boxes[i].value()
+            constraints.addMomentConstraint(constraint_type, min_moment)
+
+        # Cp constraints
+        constraints.clearCpConstraints()
+        for i in range(len(self.cptype_boxes)):
+            constraint_type = self.cptype_boxes[i].currentText()
+            min_cp = self.mincp_boxes[i].value()
+            constraints.addCpConstraint(constraint_type, min_cp)
+
+        # Additional thickness constraints
+        constraints.clearThicknessConstraints()
+        for i in range(len(self.addthickx_boxes)):
+            x = self.addthickx_boxes[i].value()
+            thickmin = self.addthickmin_boxes[i].value()/100.
+            thickmax = self.addthickmax_boxes[i].value()/100
+            constraints.addThicknessConstraint(x, thickmin, thickmax)
